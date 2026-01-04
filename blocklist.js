0e7fc045cac2ba8243d10d96b2b97b98fe149e87
Flux3tor <tusharsinha199@gmail.com>
2026-01-04 18:02:38 +0530

    Added Ad Blocking and UI Changes

---
+++ blocklist.js
@@ -0,0 +1,12 @@
+module.exports = [
+    "doubleclick.net",
+    "googlesyndication.com",
+    "googleadservices.com",
+    "adsystem.com",
+    "adservice.google.com",
+    "facebook.net",
+    "facebook.com/tr",
+    "analytics.google.com",
+    "googletagmanager.com",
+    "pixel.facebook.com"
+];
--- main.js
+++ main.js
@@ -1,4 +1,18 @@
-const { app, BrowserWindow } = require("electron");
+const { app, BrowserWindow, Menu } = require("electron");
+const path = require("path");
+const blocklist = require("./blocklist");
+
+function setupBlocking(sess) {
+  sess.webRequest.onBeforeRequest(
+    { urls: ["http://*/*", "https://*/*"] },
+    (details, callback) => {
+      const blocked = blocklist.some(domain =>
+        details.url.includes(domain)
+      );
+      callback({ cancel: blocked });
+    }
+  );
+}
 
 function createWindow() {
   const win = new BrowserWindow({
@@ -7,11 +21,48 @@ function createWindow() {
     webPreferences: {
       nodeIntegration: true,
       contextIsolation: false,
-      webviewTag: true // IMPORTANT!!
+      webviewTag: true
     }
   });
 
-  win.loadFile("index.html");
+  win.loadFile(path.join(__dirname, "index.html"));
+
+  // Attach adblocking
+  win.webContents.once("did-finish-load", () => {
+    setupBlocking(win.webContents.session);
+  });
+
+  //  REAL RIGHT-CLICK FOR WEBVIEW
+  win.webContents.on("did-attach-webview", (_event, webContents) => {
+    setupBlocking(webContents.session);
+
+    webContents.on("context-menu", (_e, params) => {
+      const menu = Menu.buildFromTemplate([
+        {
+          label: "Back",
+          enabled: webContents.canGoBack(),
+          click: () => webContents.goBack()
+        },
+        {
+          label: "Forward",
+          enabled: webContents.canGoForward(),
+          click: () => webContents.goForward()
+        },
+        { type: "separator" },
+        {
+          label: "Reload",
+          click: () => webContents.reload()
+        },
+        { type: "separator" },
+        {
+          label: "Inspect",
+          click: () => webContents.inspectElement(params.x, params.y)
+        }
+      ]);
+
+      menu.popup();
+    });
+  });
 }
 
 app.whenReady().then(createWindow);
